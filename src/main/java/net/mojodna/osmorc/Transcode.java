a88dd0a0dbeaf441ad2165a1b666b57de32ddb72
Seth Fitzsimmons <seth@mojodna.net>
2016-12-04 20:11:27 -0800

    Parallelize writes, single file output

--- src/main/java/net/mojodna/osmorc/Transcode.java
+++ src/main/java/net/mojodna/osmorc/Transcode.java
@@ -13,19 +13,21 @@ import org.apache.orc.TypeDescription;
 import org.apache.orc.Writer;
 
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.math.BigDecimal;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import static org.apache.orc.TypeDescription.*;
 
 public class Transcode {
     public static void main(String[] args) throws Exception {
-        // TODO create a single schema and write everything to that
-        // if split, it can be treated as "partitioned on type"
-        TypeDescription nodeSchema = createStruct()
-                .addField("changeset", createLong())
+        TypeDescription schema = createStruct()
                 .addField("id", createLong())
+                .addField("type", createString())
+                .addField("changeset", createLong())
                 .addField("tags", createMap(
                         createString(),
                         createString()
@@ -36,37 +38,11 @@ public class Transcode {
                 .addField("version", createLong())
                 .addField("visible", createBoolean())
                 .addField("lat", createDecimal().withScale(7).withPrecision(9))
-                .addField("lon", createDecimal().withScale(7).withPrecision(10));
-
-        TypeDescription waySchema = createStruct()
-                .addField("changeset", createLong())
-                .addField("id", createLong())
-                .addField("tags", createMap(
-                        createString(),
-                        createString()
-                ))
-                .addField("timestamp", createTimestamp())
-                .addField("uid", createLong())
-                .addField("user", createString())
-                .addField("version", createLong())
-                .addField("visible", createBoolean())
+                .addField("lon", createDecimal().withScale(7).withPrecision(10))
                 .addField("nds", createList(
                     createStruct()
                         .addField("ref", createLong())
-                ));
-
-        TypeDescription relationSchema = createStruct()
-                .addField("changeset", createLong())
-                .addField("id", createLong())
-                .addField("tags", createMap(
-                        createString(),
-                        createString()
                 ))
-                .addField("timestamp", createTimestamp())
-                .addField("uid", createLong())
-                .addField("user", createString())
-                .addField("version", createLong())
-                .addField("visible", createBoolean())
                 .addField("members", createList(
                     createStruct()
                         .addField("type", createString())
@@ -78,90 +54,84 @@ public class Transcode {
         // id can be omitted if elements are assumed to be sorted
         // user can be omitted if separate user mappings are kept (to produce a list of uids from a list of usernames)
 //        conf.set(OrcConf.BLOOM_FILTER_COLUMNS.getAttribute(), "tags,type");
-        Writer nodeWriter = OrcFile.createWriter(new Path("nodes.orc"),
-                OrcFile.writerOptions(conf).setSchema(nodeSchema));
-        Writer wayWriter = OrcFile.createWriter(new Path("ways.orc"),
-                OrcFile.writerOptions(conf).setSchema(waySchema));
-        Writer relationWriter = OrcFile.createWriter(new Path("relations.orc"),
-                OrcFile.writerOptions(conf).setSchema(relationSchema));
+        Writer writer = OrcFile.createWriter(new Path("osm.orc"),
+                OrcFile.writerOptions(conf).setSchema(schema));
 
         InputStream input = new FileInputStream(args[0]);
 
+        VectorizedRowBatch batch = schema.createRowBatch();
+
+        LongColumnVector id = (LongColumnVector) batch.cols[0];
+        BytesColumnVector type = (BytesColumnVector) batch.cols[1];
+        LongColumnVector changeset = (LongColumnVector) batch.cols[2];
+        MapColumnVector tags = (MapColumnVector) batch.cols[3];
+        TimestampColumnVector timestamp = (TimestampColumnVector) batch.cols[4];
+        LongColumnVector uid = (LongColumnVector) batch.cols[5];
+        BytesColumnVector user = (BytesColumnVector) batch.cols[6];
+        LongColumnVector version = (LongColumnVector) batch.cols[7];
+        LongColumnVector visible = (LongColumnVector) batch.cols[8];
+        DecimalColumnVector lat = (DecimalColumnVector) batch.cols[9];
+        DecimalColumnVector lon = (DecimalColumnVector) batch.cols[10];
+        ListColumnVector nds = (ListColumnVector) batch.cols[11];
+        ListColumnVector members = (ListColumnVector) batch.cols[12];
+
         OsmIterator iterator = new PbfIterator(input, true);
+        Stream<EntityContainer> entityStream = StreamSupport.stream(iterator.spliterator(), true);
 
-        VectorizedRowBatch nodeBatch = nodeSchema.createRowBatch();
-        VectorizedRowBatch wayBatch = waySchema.createRowBatch();
-        VectorizedRowBatch relationBatch = relationSchema.createRowBatch();
-
-        LongColumnVector nodeChangeset = (LongColumnVector) nodeBatch.cols[0];
-        LongColumnVector wayChangeset = (LongColumnVector) wayBatch.cols[0];
-        LongColumnVector relationChangeset = (LongColumnVector) relationBatch.cols[0];
-        LongColumnVector nodeId = (LongColumnVector) nodeBatch.cols[1];
-        LongColumnVector wayId = (LongColumnVector) wayBatch.cols[1];
-        LongColumnVector relationId = (LongColumnVector) relationBatch.cols[1];
-        MapColumnVector nodeTags = (MapColumnVector) nodeBatch.cols[2];
-        MapColumnVector wayTags = (MapColumnVector) wayBatch.cols[2];
-        MapColumnVector relationTags = (MapColumnVector) relationBatch.cols[2];
-        TimestampColumnVector nodeTimestamp = (TimestampColumnVector) nodeBatch.cols[3];
-        TimestampColumnVector wayTimestamp = (TimestampColumnVector) wayBatch.cols[3];
-        TimestampColumnVector relationTimestamp = (TimestampColumnVector) relationBatch.cols[3];
-        LongColumnVector nodeUid = (LongColumnVector) nodeBatch.cols[4];
-        LongColumnVector wayUid = (LongColumnVector) wayBatch.cols[4];
-        LongColumnVector relationUid = (LongColumnVector) relationBatch.cols[4];
-        BytesColumnVector nodeUser = (BytesColumnVector) nodeBatch.cols[5];
-        BytesColumnVector wayUser = (BytesColumnVector) wayBatch.cols[5];
-        BytesColumnVector relationUser = (BytesColumnVector) relationBatch.cols[5];
-        LongColumnVector nodeVersion = (LongColumnVector) nodeBatch.cols[6];
-        LongColumnVector wayVersion = (LongColumnVector) wayBatch.cols[6];
-        LongColumnVector relationVersion = (LongColumnVector) relationBatch.cols[6];
-        LongColumnVector nodeVisible = (LongColumnVector) nodeBatch.cols[7];
-        LongColumnVector wayVisible = (LongColumnVector) wayBatch.cols[7];
-        LongColumnVector relationVisible = (LongColumnVector) relationBatch.cols[7];
-        DecimalColumnVector lat = (DecimalColumnVector) nodeBatch.cols[8];
-        ListColumnVector nds = (ListColumnVector) wayBatch.cols[8];
-        ListColumnVector members = (ListColumnVector) relationBatch.cols[8];
-        DecimalColumnVector lon = (DecimalColumnVector) nodeBatch.cols[9];
-
-        // TODO parallelize the processing of stuff produced by the iterator
-        for (EntityContainer container : iterator) {
+        entityStream.forEach(container -> {
             OsmEntity entity = container.getEntity();
             OsmMetadata metadata = entity.getMetadata();
 
             int row;
-            int i = 0;
+
+            synchronized (batch) {
+                if (batch.size == batch.getMaxSize()) {
+                    try {
+                        writer.addRowBatch(batch);
+                        batch.reset();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+
+                row = batch.size++;
+            }
+
+            id.vector[row] = entity.getId();
+            changeset.vector[row] = metadata.getChangeset();
+            type.setVal(row, container.getType().toString().toLowerCase().getBytes());
+
+            synchronized (tags) {
+                tags.offsets[row] = tags.childCount;
+                tags.lengths[row] = entity.getNumberOfTags(); // number of key/value pairings
+                tags.childCount += tags.lengths[row];
+                tags.keys.ensureSize(tags.childCount, tags.offsets[row] != 0);
+                tags.values.ensureSize(tags.childCount, tags.offsets[row] != 0);
+            }
+
+            synchronized (tags) {
+                int i = 0;
+                for (Map.Entry<String, String> kv : OsmModelUtil.getTagsAsMap(entity).entrySet()) {
+                    ((BytesColumnVector) tags.keys).setVal((int) tags.offsets[row] + i, kv.getKey().getBytes());
+                    ((BytesColumnVector) tags.values).setVal((int) tags.offsets[row] + i, kv.getValue().getBytes());
+
+                    i++;
+                }
+            }
+
+            timestamp.time[row] = metadata.getTimestamp();
+            timestamp.nanos[row] = 0;
+
+            uid.vector[row] = metadata.getUid();
+
+            user.setVal(row, metadata.getUser().getBytes());
+
+            version.vector[row] = metadata.getVersion();
+            visible.vector[row] = 1;
 
             switch (container.getType()) {
                 default:
                 case Node:
-                    row = nodeBatch.size++;
-
-                    nodeChangeset.vector[row] = metadata.getChangeset();
-
-                    nodeId.vector[row] = entity.getId();
-
-                    nodeTags.offsets[row] = nodeTags.childCount;
-                    nodeTags.lengths[row] = entity.getNumberOfTags(); // number of key/value pairings
-                    nodeTags.childCount += nodeTags.lengths[row];
-                    nodeTags.keys.ensureSize(nodeTags.childCount, nodeTags.offsets[row] != 0);
-                    nodeTags.values.ensureSize(nodeTags.childCount, nodeTags.offsets[row] != 0);
-
-                    for (Map.Entry<String, String> kv: OsmModelUtil.getTagsAsMap(entity).entrySet()) {
-                        ((BytesColumnVector) nodeTags.keys).setVal((int) nodeTags.offsets[row] + i, kv.getKey().getBytes());
-                        ((BytesColumnVector) nodeTags.values).setVal((int) nodeTags.offsets[row] + i, kv.getValue().getBytes());
-
-                        i++;
-                    }
-
-                    nodeTimestamp.time[row] = metadata.getTimestamp();
-                    nodeTimestamp.nanos[row] = 0;
-
-                    nodeUid.vector[row] = metadata.getUid();
-
-                    nodeUser.setVal(row, metadata.getUser().getBytes());
-
-                    nodeVersion.vector[row] = metadata.getVersion();
-                    nodeVisible.vector[row] = 1;
-                    
                     OsmNode node = (OsmNode) entity;
 
                     lat.set(row, HiveDecimal.create(BigDecimal.valueOf(node.getLatitude())));
@@ -170,114 +140,46 @@ public class Transcode {
                     break;
 
                 case Way:
-                    row = wayBatch.size++;
-
-                    wayChangeset.vector[row] = metadata.getChangeset();
-
-                    wayId.vector[row] = entity.getId();
-
-                    wayTags.offsets[row] = wayTags.childCount;
-                    wayTags.lengths[row] = entity.getNumberOfTags(); // number of key/value pairings
-                    wayTags.childCount += wayTags.lengths[row];
-                    wayTags.keys.ensureSize(wayTags.childCount, wayTags.offsets[row] != 0);
-                    wayTags.values.ensureSize(wayTags.childCount, wayTags.offsets[row] != 0);
-
-                    for (Map.Entry<String, String> kv: OsmModelUtil.getTagsAsMap(entity).entrySet()) {
-                        ((BytesColumnVector) wayTags.keys).setVal((int) wayTags.offsets[row] + i, kv.getKey().getBytes());
-                        ((BytesColumnVector) wayTags.values).setVal((int) wayTags.offsets[row] + i, kv.getValue().getBytes());
-
-                        i++;
-                    }
-
-                    wayTimestamp.time[row] = metadata.getTimestamp();
-                    wayTimestamp.nanos[row] = 0;
-
-                    wayUid.vector[row] = metadata.getUid();
-
-                    wayUser.setVal(row, metadata.getUser().getBytes());
-
-                    wayVersion.vector[row] = metadata.getVersion();
-                    wayVisible.vector[row] = 1;
-                    
                     OsmWay way = (OsmWay) entity;
 
-                    nds.offsets[row] = nds.childCount;
-                    nds.lengths[row] = way.getNumberOfNodes();
-                    nds.childCount += nds.lengths[row];
-                    nds.child.ensureSize(nds.childCount, nds.offsets[row] != 0);
+                    synchronized (nds) {
+                        nds.offsets[row] = nds.childCount;
+                        nds.lengths[row] = way.getNumberOfNodes();
+                        nds.childCount += nds.lengths[row];
+                        nds.child.ensureSize(nds.childCount, nds.offsets[row] != 0);
 
-                    for (int j = 0; j < way.getNumberOfNodes(); j++) {
-                        StructColumnVector ndsStruct = (StructColumnVector) nds.child;
+                        for (int j = 0; j < way.getNumberOfNodes(); j++) {
+                            StructColumnVector ndsStruct = (StructColumnVector) nds.child;
 
-                        ((LongColumnVector) ndsStruct.fields[0]).vector[(int) nds.offsets[row] + j] = way.getNodeId(j);
+                            ((LongColumnVector) ndsStruct.fields[0]).vector[(int) nds.offsets[row] + j] = way.getNodeId(j);
+                        }
                     }
 
                     break;
 
                 case Relation:
-                    row = relationBatch.size++;
-
-                    relationChangeset.vector[row] = metadata.getChangeset();
-
-                    relationId.vector[row] = entity.getId();
-
-                    relationTags.offsets[row] = relationTags.childCount;
-                    relationTags.lengths[row] = entity.getNumberOfTags(); // number of key/value pairings
-                    relationTags.childCount += relationTags.lengths[row];
-                    relationTags.keys.ensureSize(relationTags.childCount, relationTags.offsets[row] != 0);
-                    relationTags.values.ensureSize(relationTags.childCount, relationTags.offsets[row] != 0);
-
-                    for (Map.Entry<String, String> kv: OsmModelUtil.getTagsAsMap(entity).entrySet()) {
-                        ((BytesColumnVector) relationTags.keys).setVal((int) relationTags.offsets[row] + i, kv.getKey().getBytes());
-                        ((BytesColumnVector) relationTags.values).setVal((int) relationTags.offsets[row] + i, kv.getValue().getBytes());
-
-                        i++;
-                    }
-
-                    relationTimestamp.time[row] = metadata.getTimestamp();
-                    relationTimestamp.nanos[row] = 0;
-
-                    relationUid.vector[row] = metadata.getUid();
-
-                    relationUser.setVal(row, metadata.getUser().getBytes());
-
-                    relationVersion.vector[row] = metadata.getVersion();
-                    relationVisible.vector[row] = 1;
-
                     OsmRelation relation = (OsmRelation) entity;
 
-                    members.offsets[row] = members.childCount;
-                    members.lengths[row] = relation.getNumberOfMembers();
-                    members.childCount += members.lengths[row];
-                    members.child.ensureSize(members.childCount, members.offsets[row] != 0);
+                    synchronized (members) {
+                        members.offsets[row] = members.childCount;
+                        members.lengths[row] = relation.getNumberOfMembers();
+                        members.childCount += members.lengths[row];
+                        members.child.ensureSize(members.childCount, members.offsets[row] != 0);
 
-                    for (int j = 0; j < relation.getNumberOfMembers(); j++) {
-                        StructColumnVector membersStruct = (StructColumnVector) members.child;
-                        ((BytesColumnVector) membersStruct.fields[0]).setVal((int) members.offsets[row] + j, relation.getMember(j).getType().toString().toLowerCase().getBytes());
-                        ((LongColumnVector) membersStruct.fields[1]).vector[(int) members.offsets[row] + j] = relation.getMember(j).getId();
-                        ((BytesColumnVector) membersStruct.fields[2]).setVal((int) members.offsets[row] + j, relation.getMember(j).getRole().getBytes());
+                        for (int j = 0; j < relation.getNumberOfMembers(); j++) {
+                            StructColumnVector membersStruct = (StructColumnVector) members.child;
+                            ((BytesColumnVector) membersStruct.fields[0]).setVal((int) members.offsets[row] + j, relation.getMember(j).getType().toString().toLowerCase().getBytes());
+                            ((LongColumnVector) membersStruct.fields[1]).vector[(int) members.offsets[row] + j] = relation.getMember(j).getId();
+                            ((BytesColumnVector) membersStruct.fields[2]).setVal((int) members.offsets[row] + j, relation.getMember(j).getRole().getBytes());
+                        }
                     }
 
                     break;
             }
+        });
 
-            // If the batch is full, write it out and start over.
-            if (nodeBatch.size == nodeBatch.getMaxSize()) {
-                nodeWriter.addRowBatch(nodeBatch);
-                nodeBatch.reset();
-            }
-            if (wayBatch.size == wayBatch.getMaxSize()) {
-                wayWriter.addRowBatch(wayBatch);
-                wayBatch.reset();
-            }
-            if (relationBatch.size == relationBatch.getMaxSize()) {
-                relationWriter.addRowBatch(relationBatch);
-                relationBatch.reset();
-            }
-        }
-
-        nodeWriter.close();
-        wayWriter.close();
-        relationWriter.close();
+        // flush any pending rows
+        writer.addRowBatch(batch);
+        writer.close();
     }
 }
