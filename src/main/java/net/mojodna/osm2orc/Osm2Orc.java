2007288256518c004b55e7835eb28056f703b09e
Seth Fitzsimmons <seth@mojodna.net>
2017-01-23 20:49:16 -0800

    Usage

--- src/main/java/net/mojodna/osm2orc/Osm2Orc.java
+++ src/main/java/net/mojodna/osm2orc/Osm2Orc.java
@@ -5,10 +5,16 @@ import net.mojodna.osm2orc.standalone.OsmPbf2Orc;
 
 public class Osm2Orc {
     public static void main(String[] args) throws Exception {
+        if (args.length < 2) {
+            System.err.println("Usage: osm2orc [--changeset] <input> <output>");
+            System.exit(1);
+        }
+
         if (args[0].equals("--changeset")) {
             (new OsmChangesetXml2Orc(args[1], args[2])).convert();
             return;
         }
+
         OsmPbf2Orc.convert(args[0], args[1]);
     }
 }
